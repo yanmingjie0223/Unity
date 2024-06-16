46f1b9c6ecf65ee901c40863cb14f0a10f3fa47c
yanmingjie <2386068693@qq.com>
2024-06-16 23:43:10 +0800

    update: 升级到22.3.30f1c1

--- BaseProjectFGUI/Assets/Scripts/Steamworks.NET/SteamManager.cs
+++ BaseProjectFGUI/Assets/Scripts/Steamworks.NET/SteamManager.cs
@@ -121,7 +121,7 @@ public class SteamManager : MonoBehaviour {
 		// https://partner.steamgames.com/doc/sdk/api#initialization_and_shutdown
 		m_bInitialized = SteamAPI.Init();
 		if (!m_bInitialized) {
-            Debug.LogError("Steam is not running or AppId_t error");
+            Debug.LogWarning("Steam is not running or AppId_t error");
             //Debug.LogError("[Steamworks.NET] SteamAPI_Init() failed. Refer to Valve's documentation or the comment above this line for more information.", this);
 			return;
 		}
